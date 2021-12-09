7f93d1e72fe7fcbca43b8894da29fb6c6994efb1
Hotakus <553070703@qq.com>
2021-12-09 14:44:06 +0800

    new design

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,37 @@
-﻿namespace HGMC
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+using GI.Screenshot;
+using HGMC.Source.HardwareMonitor;
+using HGMC.Source.TCP;
+
+namespace HGMC
 {
   /// <summary>
   /// Interaction logic for App.xaml
   /// </summary>
   public partial class App
   {
+    public void Test()
+    {
+      // var hardwareMonitor = new HardwareMonitor();
+      // var txt = hardwareMonitor.UpdateHardware();
+      // //Console.WriteLine(txt);
+
+      // var tcu = new TcpClientUtil();
+      // tcu.SetServerParams("10.88.171.96", 8080);
+      // tcu.Connect();
+
+      // tcu.SendMsg("Hello, I am HellGateMonitor's TCP");
+      // tcu.SendMsg(txt);
+
+      //new Thread(() => { tcu.ReceiveMsgThread(); }).Start();
+    }
+
+    public App()
+    {
+      
+    }
   }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,34 +1,104 @@
-﻿using System.Threading;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using HGMC.Source.HardwareMonitor;
 using HGMC.Source.TCP;
+using GI.Screenshot;
+using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System.Windows.Forms;
 
 namespace HGMC
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow
-  {
-
-    public void Test()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
     {
-      var hardwareMonitor = new HardwareMonitor();
-      var txt = hardwareMonitor.UpdateHardware();
-      //Console.WriteLine(txt);
 
-      var tcu = new TcpClientUtil();
-      tcu.SetServerParams("10.88.171.96", 8080);
-      tcu.Connect();
+        private readonly int _screenHeight = Screen.PrimaryScreen.Bounds.Height;
+        private readonly int _screenWidth = Screen.PrimaryScreen.Bounds.Width;
 
-      tcu.SendMsg("Hello, I am HellGateMonitor's TCP");
-      tcu.SendMsg(txt);
+        public MainWindow()
+        {
+            InitializeComponent();
 
-      new Thread(() => { tcu.ReceiveMsgThread(); }).Start();
+            var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+
+            theme.SetBaseTheme(Theme.Light);
+            theme.PrimaryMid = new ColorPair(Colors.Teal, Colors.WhiteSmoke);
+            paletteHelper.SetTheme(theme);
+
+            var image = Screenshot.CaptureAllScreens();
+
+            var t = new Thread(() =>
+            {
+                while (true)
+                {
+                    image = Screenshot.CaptureRegion(new Rect(0, 0, _screenWidth, _screenHeight));
+                    Thread.Sleep(1);
+                }
+            });
+            t.Start();
+
+            var t2 = new Thread(() =>
+            {
+                while (true)
+                {
+                    Dispatcher.BeginInvoke(DispatcherPriority.Background, (Action)delegate () { scrCapImg.Source = image; });
+                    Thread.Sleep(1);
+                }
+            });
+            //t2.Start();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        bool flag = false;
+        private void test_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag)
+            {
+                ScreenCaptureArea.Visibility = Visibility.Visible;
+                flag = false;
+            }
+            else
+            {
+                ScreenCaptureArea.Visibility = Visibility.Collapsed;
+                flag = true;
+            }
+        }
+
+        private void HardwareArea_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+            eventArg.RoutedEvent = MouseWheelEvent;
+            eventArg.Source = sender;
+            HardwareArea.RaiseEvent(eventArg);
+        }
     }
-    
-    public MainWindow()
-    {
-      InitializeComponent();
-    }
-  }
 }
